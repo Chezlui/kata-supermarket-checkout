e1ffb2fb8c9bb4e0be86a951d6b2052404a48ff9
emilybache <emily.bache@praqma.com>
2018-11-26 14:08:36 +0100

    second test

--- java/src/test/java/dojo/supermarket/model/WhenCheckingOutArticlesAtTheSupermarket.java
+++ java/src/test/java/dojo/supermarket/model/WhenCheckingOutArticlesAtTheSupermarket.java
@@ -1,28 +1,50 @@
 package dojo.supermarket.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
 
 public class WhenCheckingOutArticlesAtTheSupermarket {
-    SupermarketCatalog catalog = new DummyCatalog();
-    Teller teller = new Teller(catalog);
-    ShoppingCart theCart = new ShoppingCart();
+    private SupermarketCatalog catalog;
+    private Teller teller;
+    private ShoppingCart theCart;
+    private Product toothbrush;
+    private Product rice;
+
+    @Before
+    public void setUp() {
+        catalog = new DummyCatalog();
+        teller = new Teller(catalog);
+        theCart = new ShoppingCart();
+
+        toothbrush = new Product("toothbrush");
+        catalog.addProduct(toothbrush, 0.99);
+        rice = new Product("rice");
+        catalog.addProduct(rice, 2.99);
+    }
 
     @Test
     public void an_empty_shopping_cart_should_cost_nothing() {
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        assertThat(receipt.getTotalPrice(), equalTo(0.00));
+        assertEquals(0.00, receipt.getTotalPrice(), 0.001);
+
     }
 
     @Test
-    public void one_toothbrush_costs_99() {
-        Product toothbrush = new Product("toothbrush");
-        catalog.addProduct(toothbrush, 0.99);
+    public void one_normal_item() {
         theCart.addItem(toothbrush);
         Receipt receipt = teller.checksOutArticlesFrom(theCart);
-        assertThat(receipt.getTotalPrice(), equalTo(0.99));
+        assertEquals(0.99, receipt.getTotalPrice(), 0.001);
+    }
 
+    @Test
+    public void two_normal_items() {
+        theCart.addItem(toothbrush);
+        theCart.addItem(rice);
+        Receipt receipt = teller.checksOutArticlesFrom(theCart);
+        assertEquals(0.99 + 2.99, receipt.getTotalPrice(), 0.001);
     }
 }
